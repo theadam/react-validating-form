a603ba4cef1962c07a9fd5de9bd952332eb27f22
Adam Nalisnick <theadam4257@gmail.com>
2014-09-03 22:14:44 -0400

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,60 @@
+var Schema = require('node-schema');
+var Field = Schema.Field;
+var str = require('string-validator');
+var React = require('react');
+var ValidatingForm = require('../../index').ValidatingForm;
+var ValidatingInput = require('../../index').ValidatingInput;
+var Well = require('react-bootstrap').Well;
+
+var userSchema = Schema({
+  username: Field.required('Username is required', {
+    'Username must only contain letters, numbers, _, and -': str.matches(/^[a-zA-Z\-_]*$/),
+    'Username must be at least 5 characters long': str.isLength(5)
+  }),
+  password: Field.required('Password is required', {
+    'Password must be at least 5 characters long': str.isLength(5)
+  }),
+  passwordCheck: Field.optional({
+    'Passwords must match': function(val, options, object){
+      return val == object.password;
+    }
+  })
+});
+
+var Example1 = React.createClass({
+
+  handleSubmit: function(){
+    alert(JSON.stringify(this.refs.form.getValue()));
+  },
+
+  render: function(){
+    var wrapperClassName='col-xs-12';
+    return (
+      <Well className="example">
+        <ValidatingForm
+          schema={userSchema}
+          onSubmit={this.handleSubmit}
+          submitLabel="Register"
+          ref='form'
+          horizontal>
+          <ValidatingInput
+            field='username'
+            placeholder='Enter Username'
+            wrapperClassName={wrapperClassName}/>
+          <ValidatingInput
+            field='password'
+            type='password'
+            placeholder='Enter Password'
+            wrapperClassName={wrapperClassName}/>
+          <ValidatingInput
+            field='passwordCheck'
+            type='password'
+            placeholder='Re-enter Password'
+            wrapperClassName={wrapperClassName}/>
+        </ValidatingForm>
+      </Well>
+    );
+  }
+});
+
+React.renderComponent(<Example1 />, document.getElementById('content'));
